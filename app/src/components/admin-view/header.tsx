f53553d717999c166b034e0ffa531e200e578d49
unknown <Hamzakhan70>
2024-12-14 13:19:56 +0500

    layout of admin is created completly

--- app/src/components/admin-view/header.tsx
+++ app/src/components/admin-view/header.tsx
@@ -1,10 +1,27 @@
-function AdminHeader(){
-    return(
-        <>
-        
-        <h1>Admin Header</h1>
-        </>
-    )
+import { AlignJustify, LogOut } from "lucide-react";
+import { Dispatch, SetStateAction } from "react";
+import { Button } from "../ui/button";
+interface AHeaderInterface {
+  setOpen: Dispatch<SetStateAction<Boolean>>;
+}
+function AdminHeader({ setOpen }) {
+  return (
+    <header className="flex items-center justify-between px-4 py-3 bg-background border-b">
+      <Button onClick={() => setOpen(true)} className="lg:hidden sm:block">
+        <AlignJustify />
+        <span className="sr-only">Toggle Menu</span>
+      </Button>
+      <div className="flex flex-1 justify-end">
+        <Button
+          //   onClick={handleLogout}
+          className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow"
+        >
+          <LogOut />
+          Logout
+        </Button>
+      </div>
+    </header>
+  );
 }
 
-export default AdminHeader
+export default AdminHeader;
--- app/src/components/admin-view/layout.tsx
+++ app/src/components/admin-view/layout.tsx
@@ -8,14 +8,11 @@ function AdminLayout() {
 
   return (
     <div className="flex min-h-screen w-full">
-        <h1>Admin</h1>
       {/* admin sidebar */}
-      {/* <AdminSideBar open={openSidebar} setOpen={setOpenSidebar} /> */}
-      <AdminSideBar  />
+      <AdminSideBar open={openSidebar} setOpen={setOpenSidebar} />
       <div className="flex flex-1 flex-col">
         {/* admin header */}
-        {/* <AdminHeader setOpen={setOpenSidebar} /> */}
-        <AdminHeader  />
+        <AdminHeader setOpen={setOpenSidebar} />
         <main className="flex-1 flex-col flex bg-muted/40 p-4 md:p-6">
           <Outlet />
         </main>
