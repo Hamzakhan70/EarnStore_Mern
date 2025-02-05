4e9137e5561b780da3081b10ede3bc3d8a523db3
unknown <Hamzakhan70>
2025-02-05 11:48:16 +0500

    vercel url is updated in env

--- api/server.js
+++ api/server.js
@@ -23,7 +23,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(
   cors({
-    origin: process.env.URL,
+    origin: process.env.URL || "http://localhost:3000",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
