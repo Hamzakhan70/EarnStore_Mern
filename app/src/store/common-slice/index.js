3a536e0ed48230e4557dd98a392020f39a7025d8
Hamzakhan70 <hamzaarifkhan70@gmail.com>
2025-02-06 23:07:36 +0500

    authentication is working

--- app/src/store/common-slice/index.js
+++ app/src/store/common-slice/index.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 const BASE_URL =
-  import.meta.env.VITE_BACKEND_URL || "http://localhost:5000/api";
+  import.meta.env.VITE_BACKEND_URL;
 const initialState = {
   isLoading: false,
   featureImageList: [],
