18ff646503d76a6697364a102d17f26f659c4fe9
unknown <Hamzakhan70>
2025-02-06 13:02:38 +0500

    try

--- app/src/store/auth-slice/index.js
+++ app/src/store/auth-slice/index.js
@@ -7,6 +7,7 @@ const initialState = {
   isLoading: true,
   user: null,
 };
+console.log("Backend URL:", BASE_URL);
 export const registerUser = createAsyncThunk(
   "/auth/register",
   async (formData) => {
@@ -16,12 +17,15 @@ export const registerUser = createAsyncThunk(
     return response;
   }
 );
-console.log("Backend URL:", BASE_URL);
 export const loginUser = createAsyncThunk("/auth/login", async (formData) => {
   // const response = await axios.post(`${BASE_URL}/auth/login`, formData, {
-  const response = await axios.post(`https://earnstoremern-production.up.railway.app/api/auth/login`, formData, {
-    withCredentials: true,
-  });
+  const response = await axios.post(
+    `https://earnstoremern-production.up.railway.app/api/auth/login`,
+    formData,
+    {
+      withCredentials: true,
+    }
+  );
   return response.data;
 });
 export const logoutUser = createAsyncThunk(
